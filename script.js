241b2e35bf23cb3e46d54dd11f236c0507bfc228
manshal Bhagat <93897590+manshal01@users.noreply.github.com>
2023-07-28 22:14:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ let displayValue = '';
+
+function appendToDisplay(val) {
+  displayValue += val;
+  document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = '';
+  document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+  try {
+    const result = eval(displayValue);
+    document.getElementById('display').value = result;
+    displayValue = result.toString();
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+    displayValue = '';
+  }
+}
